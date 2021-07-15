25ce99950a93add71a5be34f5b3397052cfb7e2e
CharukaHS <charuka@protonmail.com>
2021-07-15 12:40:27 +0530

    Create users.auth table

--- backend/database/boot/users.sql
+++ backend/database/boot/users.sql
@@ -1,9 +1,17 @@
-CREATE schema if not exists users;
-CREATE TABLE if not exists users.data(
-  id integer primary key generated always AS identity,
+-- @block create schema
+create schema if not exists users;
+-- @block create users.data
+create table if not exists users.data(
+  id integer primary key generated always as identity,
   email varchar(255) unique not null,
   fname varchar(255) not null,
   lname varchar(255) not null,
   created_at timestamp not null,
   created_by integer references users.data(id)
+);
+-- @block create users.schema
+create table if not exists users.auth(
+  id integer references users.data(id),
+  username varchar(255) primary key,
+  pwd varchar(255) not null
 );
