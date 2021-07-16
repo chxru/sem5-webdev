98d5b90373210bec4e073131341db8dfdec09b83
CharukaHS <charuka@protonmail.com>
2021-07-16 19:08:05 +0530

    Add notification overlay

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,10 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import Overlay from "../components/overlay";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Overlay>
+        <Component {...pageProps} />
+      </Overlay>
     </ChakraProvider>
   );
 }
