80a1f07edafba0bed1277adc75b6eea779919c0c
CharukaHS <charuka@protonmail.com>
2021-07-16 19:21:54 +0530

    Enable cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { logger } from "./util/logger";
 
 // routes
@@ -7,6 +8,7 @@ import authroutes from "./routes/auth";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/auth", authroutes);
 
