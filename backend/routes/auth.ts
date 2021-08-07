e162cf34e146cf1861675b672ed9a47715411120
CharukaHS <charuka@protonmail.com>
2021-08-08 04:54:23 +0530

    Moved express validator schemas to separate folder

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,62 +1,10 @@
 import express, { Router } from "express";
-import { checkSchema, Schema, validationResult } from "express-validator";
+import { checkSchema, validationResult } from "express-validator";
 
 import { HandleLogin, HandleRegister } from "../controllers/auth";
+import { signin_schema, signup_schema } from "./schemas/auth";
 
 import { logger } from "../util/logger";
-
-// schemas
-const signup_schema: Schema = {
-  email: {
-    in: "body",
-    errorMessage: "Invalid email",
-    isEmail: true,
-    trim: true,
-  },
-  fname: {
-    in: "body",
-    isString: true,
-    errorMessage: "Invalid First Name",
-    trim: true,
-  },
-  lname: {
-    in: "body",
-    isString: true,
-    errorMessage: "Invalid Last Name",
-    trim: true,
-  },
-  username: {
-    in: "body",
-    isString: true,
-    errorMessage: "Invalid username",
-    isLength: {
-      errorMessage: "Username must be atleast 5 letters",
-      options: {
-        min: 5,
-      },
-    },
-    trim: true,
-  },
-  password: {
-    in: "body",
-    isStrongPassword: true,
-    errorMessage: "Password isn't strong enough",
-  },
-};
-
-const signin_schema: Schema = {
-  username: {
-    in: "body",
-    errorMessage: "Invalid email",
-    isEmail: true,
-    trim: true,
-  },
-  password: {
-    in: "body",
-    isString: true,
-    errorMessage: "Invalid password",
-  },
-};
 
 const router = Router();
 
