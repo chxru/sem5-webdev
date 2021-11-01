4414e1e0384769b76774ad57333a6930488fdd24
Charuka Samarakoon <charuka@protonmail.com>
2021-11-02 02:59:16 +0530

    UI for bed ticket attachment init

--- frontend/components/bedticket/new.tsx
+++ frontend/components/bedticket/new.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   Button,
+  Flex,
   FormControl,
   FormHelperText,
   FormLabel,
@@ -12,16 +13,23 @@ import {
   ModalHeader,
   ModalOverlay,
   Select,
+  Table,
+  Tbody,
+  Td,
   Textarea,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
+import { useDropzone } from "react-dropzone";
 
 import AuthContext from "contexts/auth-context";
 import NotifyContext from "contexts/notify-context";
 
 import { ApiRequest } from "util/request";
 
-import type { API, DB } from "@sem5-webdev/types";
+import type { DB } from "@sem5-webdev/types";
 
 interface newProps {
   isOpen: boolean;
@@ -40,6 +48,38 @@ const NewRecord: React.FC<newProps> = ({ isOpen, onClose, bid, refresh }) => {
     reset,
     formState: { errors },
   } = useForm<Omit<DB.Patient.BedTicketEntry, "created_at">>();
+
+  const [acceptedFiles, setacceptedFiles] = useState<File[]>([]);
+  const onDropAccepted = (file: File[]) => {
+    // prevent duplications
+    let duplicate = false;
+    for (const f of acceptedFiles) {
+      if (f.name == file[0].name && f.size == file[0].size) {
+        duplicate = true;
+        break;
+      }
+    }
+
+    if (duplicate) {
+      notify.NewAlert({
+        status: "warning",
+        msg: "Dupicate attachment",
+      });
+      return;
+    }
+
+    setacceptedFiles((f) => [...f, file[0]]);
+  };
+
+  const onDropRejected = () => {
+    notify.NewAlert({ msg: "Only images are supported", status: "error" });
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDropAccepted,
+    onDropRejected,
+  });
 
   const OnSubmit = async (value: DB.Patient.BedTicketEntry) => {
     const { success, err } = await ApiRequest({
@@ -69,11 +109,12 @@ const NewRecord: React.FC<newProps> = ({ isOpen, onClose, bid, refresh }) => {
 
     reset();
 
+    setacceptedFiles([]);
     onClose();
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="2xl">
       <ModalOverlay />
 
       <ModalContent>
@@ -98,6 +139,61 @@ const NewRecord: React.FC<newProps> = ({ isOpen, onClose, bid, refresh }) => {
             </FormControl>
 
             <FormControl>
+              <FormLabel>Attachments</FormLabel>
+              <Flex
+                {...getRootProps()}
+                marginY="4"
+                paddingY="4"
+                paddingX="6"
+                bg="gray.100"
+                rounded="md"
+              >
+                <input {...getInputProps()} />
+                <p>Drag and drop attachments here, or click to select files</p>
+              </Flex>
+
+              {acceptedFiles.length != 0 && (
+                <Table variant="simple" size="sm">
+                  <Thead>
+                    <Tr>
+                      <Th>Filename</Th>
+                      <Th>Size</Th>
+                      <Th></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {acceptedFiles.map((file, i) => {
+                      return (
+                        <Tr key={file.name + file.size}>
+                          <Td>{file.name}</Td>
+                          <Td>
+                            {file.size < 1000000
+                              ? (file.size / 1000).toFixed(1) + " KB"
+                              : (file.size / 1000000).toFixed(1) + " MB"}
+                          </Td>
+                          <Td>
+                            <Button
+                              colorScheme="red"
+                              onClick={() => {
+                                setacceptedFiles((files) => {
+                                  const temp = [...files];
+                                  temp.splice(i, 1);
+                                  return temp;
+                                });
+                              }}
+                            >
+                              Remove
+                            </Button>
+                          </Td>
+                        </Tr>
+                      );
+                    })}
+                  </Tbody>
+                </Table>
+              )}
+            </FormControl>
+
+            <FormControl>
               <FormLabel>Notes</FormLabel>
               <Textarea {...register("note")} />
             </FormControl>
@@ -111,6 +207,7 @@ const NewRecord: React.FC<newProps> = ({ isOpen, onClose, bid, refresh }) => {
               type="reset"
               onClick={() => {
                 reset();
+                setacceptedFiles([]);
                 onClose();
               }}
             >
