c6b8d95425c4b1303edfb4e3ed3cc784bd09a904
Charuka Samarakoon <charuka@protonmail.com>
2021-11-02 15:32:10 +0530

    Add logger function

---
+++ frontend/util/logger.ts
@@ -0,0 +1,29 @@
+import chalk from "chalk";
+
+type loglevel = "info" | "success" | "error" | "debug";
+
+export const logger = async (
+  log: string,
+  level: loglevel = "debug"
+): Promise<void> => {
+  const d = new Date();
+  const t = d.toTimeString().split(" ")[0];
+  switch (level) {
+    case "error":
+      console.log(t, chalk.black.bgRed("ERROR:"), log);
+      break;
+
+    case "info":
+      console.log(t, chalk.black.bgYellow("INFO:"), log);
+      break;
+
+    case "success":
+      console.log(t, chalk.black.bgGreen("SUCCESS:"), log);
+      break;
+
+    case "debug":
+      if (process.env.PRODUCTION !== "true")
+        console.log(t, chalk.black.bgWhite("DEBUG:"), log);
+      break;
+  }
+};
